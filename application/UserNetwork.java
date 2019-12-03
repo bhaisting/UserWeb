50475b749e61f82797c658027e96c9408ef4fa9e
Kennedy <Kennedy@10.141.142.153>
2019-12-03 16:09:53 -0600

    UserNode and UserNetwork

--- application/UserNetwork.java
+++ application/UserNetwork.java
@@ -1,5 +1,67 @@
 package application;
 
+import java.util.LinkedList;
+
 public class UserNetwork implements UserNetworkADT {
-public UserNetwork() {}
+	LinkedList<UserNode> userList;
+	
+public UserNetwork() {
+	this.userList = new LinkedList<UserNode>();
+	
+	
+}
+
+@Override
+public void setFriend(String username, String username2) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void deleteFriend(String username, String username2) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void createUser(String username) {					//Might want to add boolean flag to show if user already created
+	boolean alreadyPresent = false;
+	for(int i = 0; i < this.userList.size(); i++) {
+		if(this.userList.get(i).getUsername().equals(username)) {
+			alreadyPresent = true;
+			return;
+		}
+	}
+	UserNode newUser = new UserNode(username);
+	this.userList.add(newUser);
+	return;
+}
+
+@Override
+public void deleteUser(String username) {					//Same as above
+	boolean notPresent = false;
+	for(int i = 0; i < this.userList.size(); i++) {
+		if(this.userList.get(i).getUsername().equals(username)) {
+			this.userList.remove(i);
+			return;
+		}
+	}
+	notPresent = true;
+	return;
+}
+
+@Override
+public LinkedList<UserNode> getUserList() {
+	return this.userList;
+}
+
+@Override
+public UserNode getUser(String username) {
+	for(int i = 0; i < this.userList.size(); i++) {
+		if(this.userList.get(i).getUsername().equals(username)) {
+			return this.userList.get(i);
+		}
+	}
+	return null;
+}
 }
--- application/UserNetworkADT.java
+++ application/UserNetworkADT.java
@@ -1,5 +1,26 @@
 package application;
 
-public interface UserNetworkADT {
+import java.util.LinkedList;
 
-}
+public interface UserNetworkADT {
+	LinkedList<UserNode> userList = new LinkedList<UserNode>();
+	
+
+	public void setFriend(String username, String username2);
+	
+	
+	public void deleteFriend(String username, String username2);
+	
+	
+	public void createUser(String username);
+	
+	
+	public void deleteUser(String username);
+	
+	
+	public LinkedList<UserNode> getUserList();
+	
+	
+	public UserNode getUser(String username);
+	}
+
